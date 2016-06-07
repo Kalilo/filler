725d43df3bd6fa65dbde757e71c2a461c036d5f1
Gregory HAVENGA <ghavenga@e5r7p4.jnb.42.fr>
2016-06-07 08:59:22 +0200

    Fixes

--- read.c
+++ read.c
@@ -6,7 +6,7 @@
 /*   By: khansman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/06 10:23:04 by khansman          #+#    #+#             */
-/*   Updated: 2016/06/06 11:45:29 by khansman         ###   ########.fr       */
+/*   Updated: 2016/06/06 14:45:53 by ghavenga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,21 @@ static int	process_pos(int lines, int *size, char *line, t_data *map)
 	char	**l;
 
 	l = ft_strsplit(line, ' ');
-	if (line == 1)
+	if (lines == 1)
+	{
 		if (l[2] = "p1")
 			map.player = 1;
 		else
 			map.player = 2;
-	if (line == 2)
+	}
+	if (lines == 2) 
 	{
 		map.map.y = ft_atoi(l[1]);
 		map.map.x = ft_atoi(l[2]);
 		size += map.map.y;
 	}
 	if (2 < line && line < size)
-		store_arr(&map.map, line, lines - 3);//write function
+		store_arr(&map.map, line, lines - 3);
 	if (line == size && map.piece_x == 0)
 	{
 		map.piece.y = ft_atoi(l[1]);
@@ -44,18 +46,18 @@ int			read_input(int fd, t_data *map)
 {
 	int		done;
 	int		size;
-	int		line;
+	int		line_no;
 	char	*line;
 
 	done = 0;
 	size = 4;
-	line = 1;
+	line_no = 1;
 	while(!(done))
 	{
 		if (!(getnextline(fd, &line)))
 			return (0);
-		process_pos(line, &size, map);
-		lines++;
+		process_pos(line_no, &size, line, map);
+		line_no++;
 		free(line);
 		if (line == size)
 			done = 1;
