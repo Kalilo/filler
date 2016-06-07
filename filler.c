bdc5568551703d3707b765f9d1613349472bc1bc
Gregory HAVENGA <ghavenga@e5r7p4.jnb.42.fr>
2016-06-07 16:36:28 +0200

    Debug Code

--- filler.c
+++ filler.c
@@ -4,6 +4,7 @@ int			main(void)
 {
 	t_data	gamedata;
 
+	debugfd = open("debugdump.txt", O_APPEND | O_WRONLY); //DEBUGFD
 	if(!(read_input(0, &gamedata)))
 				return (0);
 	write_map_data(gamedata);
