da6d4f4348eb7d359cd856c3fa418d81849dc41e
Gregory HAVENGA <ghavenga@e5r7p4.jnb.42.fr>
2016-06-09 16:50:22 +0200

    Debugging

--- store_arr.c
+++ store_arr.c
@@ -6,16 +6,18 @@ int		ft_malloc_arr(t_grid *grid)
 
 	k = 0;
 	grid->g = (char **)ft_strnew((grid->y + 1) * sizeof(char *));
-	ft_putstr_fd("store_arr.c\tft_malloc_arr:\tAllocated grid->g\n", debugfd);
+	ft_putstr_fd("ft_malloc_arr:\tAllocated grid->g\n", debugfd);
+		ft_putstr_fd("ft_malloc_arr:\tAllocating grid->g[", debugfd);
 	while (k < grid->y)
 	{
-		ft_putstr_fd("store_arr.c\tft_malloc_arr:\tAllocating grid->g[", debugfd);
+		ft_putstr_fd(" ", debugfd);
 		ft_putnbr_fd(k, debugfd);
-		ft_putstr_fd("]\n", debugfd);
+		ft_putstr_fd(" ", debugfd);
 		grid->g[k] = (char *)ft_strnew((grid->x + 1) * sizeof(char));
 		k++;
 	}
-		ft_putstr_fd("store_arr.c\tft_malloc_arr:\tReturning 1\n\n", debugfd);
+		ft_putstr_fd("]\n", debugfd);
+		ft_putstr_fd("ft_malloc_arr:\tReturning 1\n", debugfd);
 	return (1);
 }
 //store_arr(char **arr, int columns, int rows, int cur_line)
@@ -23,20 +25,24 @@ int		store_arr(t_grid *grid, char *line, int cur_line)
 {
 	int		k;
 
+		ft_putstr_fd("\nENTERING FILE:store_arr.c\n", debugfd);
 	k = 0;
-		ft_putstr_fd("store_arr.c\tstore_arr:\tint 'cur_line' = ", debugfd);
+		ft_putstr_fd("store_arr:\tint 'cur_line' = ", debugfd);
 		ft_putnbr_fd(cur_line, debugfd);
 		ft_putstr_fd("\n", debugfd);
 	if (cur_line == 0)
 		ft_malloc_arr(grid);
+	ft_putstr_fd("store_arr:\tReading line[k] to grid->g[", debugfd);
 	while (k < grid->x)
 	{
 		grid->g[cur_line][k] = line[k];
-		ft_putstr_fd("store_arr.c\tstore_arr:\tReading line[k] to grid->g[", debugfd);
+		ft_putstr_fd(" ", debugfd);
 		ft_putnbr_fd(k, debugfd);
-		ft_putstr_fd("]\n", debugfd);
+		ft_putstr_fd(" ", debugfd);
 		k++;
 	}
-	ft_putstr_fd("store_arr.c\tstore_arr:\tReturning 1\n\n", debugfd);
+		ft_putstr_fd("]\n", debugfd);
+	ft_putstr_fd("store_arr:\tReturning 1\n", debugfd);
+	ft_putstr_fd("EXITING FILE:store_arr.c\n\n", debugfd);
 	return (1);
 }
