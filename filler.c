3d572ad683b95e1917533fed16eb40fd3afd1e63
Kalilo HANSMANN <khansman@e5r8p1.jnb.42.fr>
2016-06-11 11:28:25 +0200

    created loop

--- filler.c
+++ filler.c
@@ -3,13 +3,16 @@
 int			main(void)
 {	
 	static t_data	gamedata;
-
+	int		k;
+	
+	k = 1;
 	debugfd = open("debugdump.txt", O_WRONLY); //DEBUGFD
-	if(!(read_input(0, &gamedata)))
-				return (0);
-	write_map_data(&gamedata);
-	if (!(scan_arr(&gamedata)))
-		return (0);
-	write_map_data(&gamedata);
-
+	while (k > 0)
+	{
+		if(!(read_input(0, &gamedata)))
+			return (0);
+		write_map_data(&gamedata);
+		k = scan_arr(&gamedata);
+	}
+	return (0);
 }
