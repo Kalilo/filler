cdd7e23c31dec1aac883672f63cb8ed7ddfe56d7
Gregory HAVENGA <ghavenga@e5r8p3.jnb.42.fr>
2016-06-11 14:22:04 +0200

    SOOOO CLOSE

--- basic_math.c
+++ basic_math.c
@@ -2,6 +2,7 @@
 
 int		ft_abs(int num)
 {
+	dprintf(debugfd, "ft_abs\n");
 	if (num > 0)
 		return (num);
 	return (-num);
@@ -9,6 +10,7 @@ int		ft_abs(int num)
 
 int		ft_is_foe(int player, char pos)
 {
+//	dprintf(debugfd, "ft_is_foe\n");
 	if (player == 1 && (pos == 'x' || pos == 'X'))
 		return (1);
 	if (player == 2 && (pos == 'o' || pos == 'O'))
@@ -22,6 +24,7 @@ int		ft_sqrt(int	num)
 {
 	int	k;
 	
+//	dprintf(debugfd, "ft_sqrt\n");
 	k = 1;
 	while (k * k < num)
 		k++;
@@ -30,6 +33,7 @@ int		ft_sqrt(int	num)
 
 int		ft_dist(t_coord a, t_coord b)
 {
+//	dprintf(debugfd, "ft_dist\n");
 	return (ft_sqrt(((a.x - b.x) * (a.x - b.x)) + 
 		((a.y - b.y) * (a.y - b.y))));
 }
--- result.c
+++ result.c
@@ -2,6 +2,7 @@
 
 int		give_result(t_coord best)
 {
+	dprintf(debugfd, "ft_give_result y=%i x=%i\n", best.y, best.x);
 	ft_putstr_fd(ft_itoa(best.y), 1);
 	ft_putstr_fd(" ", 1);
 	ft_putstr_fd(ft_itoa(best.x), 1);
